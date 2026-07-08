70374a12bea6281faaddfa00b0fbd8043d571346
anonymouspilotbr <ybrasileiro6@gmail.com>
2026-07-08 18:54:03 -0300

    [v004] Reactive Form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,47 +1,53 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
+export class LoginComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  loginForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [ Validators.email, Validators.required ]
+    }),
+    password: new FormControl('', {
+      validators: [ Validators.required, Validators.minLength(6) ]
+    })
+  });
 
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
-
-      if (savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-        const savedEmail = loadedFormData.email;
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-      }
-
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email })),
-      });
-
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-
-    });
+  get emailIsInvalid() {
+    return this.loginForm.controls.email.touched && this.loginForm.controls.email.dirty && this.loginForm.controls.email.invalid;
   }
 
-  onSubmit(formData: NgForm) {
-    if (formData.form.invalid) {
-      return;
+  get passwordIsInvalid() {
+    return this.loginForm.controls.password.touched && this.loginForm.controls.password.dirty && this.loginForm.controls.password.invalid;
+  }
+
+  ngOnInit() {
+    const savedForm = localStorage.getItem('saved-login-form');
+
+    if(savedForm){
+      const loadedForm = JSON.parse(savedForm);
+      this.loginForm.patchValue({
+        email: loadedForm.email
+      })
     }
 
-    const entEmail = formData.form.value.email;
-    const entPassword = formData.form.value.password;
+    const subscription = this.loginForm.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: value => {
+        window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}));
+      }
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 
-    formData.form.reset();
+  onSubmit() {
+    const entEmail = this.loginForm.value.email;
+    const entPassword = this.loginForm.value.password;
   }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [ Validators.email, Validators.required ]
+    }),
+    password: new FormControl('', {
+      validators: [ Validators.required, Validators.minLength(6) ]
+    })
+  })
+
+  get emailIsInvalid() {
+    return this.signupForm.controls.email.touched && this.signupForm.controls.email.dirty && this.signupForm.controls.email.invalid;
+  }
+
+  get passwordIsInvalid() {
+    return this.signupForm.controls.password.touched && this.signupForm.controls.password.dirty && this.signupForm.controls.password.invalid;
+  }
+
+  onSubmit() {
+    const entEmail = this.signupForm.value.email;
+    const entPassword = this.signupForm.value.password;
+    console.log(entEmail, entPassword);
+  }
+
+  onReset() {
+    this.signupForm.controls.email.setValue('');
+    this.signupForm.controls.password.setValue('');
+  }
+
+}
